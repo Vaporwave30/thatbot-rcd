a0528778179dbf1f9f98845dfb7188cf95173b20
plunger00 <48886746+plunger00@users.noreply.github.com>
2019-03-27 00:56:26 +0800

    fixed some bugs
    fixed typo and the time wait, also fixed a bug after i tested with api

--- py-rcd/example.py
+++ py-rcd/example.py
@@ -1,7 +1,8 @@
 import requests
+import time
 
 headers = {"X-Api-Key": ""} #put your api key here
-scrope = "login" #put the scope here
+scope = "login" #put the scope here
 
 def create_task():
     print("Creating task...")
@@ -21,10 +22,11 @@ def task_result():
     task_result_url = "https://roblox.developer-variety.com/api/rcd/task-result"
     task_id = create_task()
     if task_id != False:
-        params = {"taskID": task_id}
+        params = {"taskId": task_id}
+        time.sleep(10) #wait for 10 seconds to make sure it's solved
         r = requests.post(url=task_result_url, headers=headers, params=params).json()
         if r["status"] == "success":
-            fctoken = r["data"]["fctoken"]
+            fctoken = r["data"]["value"]
             print("Got fctoken: {}".format(fctoken))
         elif r["status"] == "error":
             print("Task result error -> {}".format(r["data"]["message"]))
